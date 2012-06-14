39785c409c6eda62aed1231747e7459343874376
Jeff Weiss <jeff.weiss@puppetlabs.com>
2012-06-14 08:08:12 -0700

    example 1-4

--- ch01/game.rb
+++ ch01/game.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
 
+# before we process a route, we'll set the response as
+# plain text and set up an array of viable moves that
+# a player (and the computer) can perform
+before do
+  content_type :txt
+  @defeat = { :rock => :scissors,
+              :paper => :rock,
+              :scissors => :paper
+            }
+  @throws = @defeat.keys
+end
+
 get '/throw/:type' do
   # play here
 end
