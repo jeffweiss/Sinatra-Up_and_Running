5453eb5e05cfc8c6e7c83fda366b612cffc15d51
Jeff Weiss <jeff.weiss@puppetlabs.com>
2012-09-25 14:39:29 -0700

    added example 2.34

---
+++ ch02/example2_34.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+
+configure do
+  enable :sessions
+end
+
+before do
+  content_type :txt
+end
+
+get '/set' do
+  session[:foo] = Time.now
+  "Session value set."
+end
+
+get '/fetch' do
+  "Session value: #{session[:foo]}"
+end
+
+get '/logout' do
+  session.clear
+  redirect '/fetch'
+end
