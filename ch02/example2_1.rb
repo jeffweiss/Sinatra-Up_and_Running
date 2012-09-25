9e5675fb0c0825b11179b4d52e285c900ed2552b
Jeff Weiss <jeff.weiss@puppetlabs.com>
2012-09-25 00:01:17 -0700

    add example 2.1

---
+++ ch02/example2_1.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+
+get '/' do
+  "Triggered via GET"
+end
+
+post '/' do
+  "Triggered via POST"
+end
+
+put '/' do
+  "Triggered via PUT"
+end
+
+delete '/' do
+  "Triggered via DELETE"
+end
+
+patch '/' do
+  "Triggered via PATCH"
+end
+
+options '/' do
+  "Triggered via OPTIONS"
+end
