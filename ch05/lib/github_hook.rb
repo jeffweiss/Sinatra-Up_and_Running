b567d499ba0ff67413d438536bd433ef6e87665c
Jeff Weiss <jeff.weiss@puppetlabs.com>
2012-09-26 10:44:35 -0700

    updated to example 5.13

--- ch05/lib/github_hook.rb
+++ ch05/lib/github_hook.rb
@@ -2,11 +2,18 @@ require 'sinatra/base'
 require 'time'
 
 class GithubHook < Sinatra::Base
+  set(:autopull) { production? }
+
   post '/update' do
     app.settings.reset!
     load app.settings.app_file
 
     content_type :txt
-    "ok"
+    if settings.autopull?
+      # Pipe stderr to stdout to make sure we display everything
+      `git pull 2>&1`
+    else
+      "ok"
+    end
   end
 end
