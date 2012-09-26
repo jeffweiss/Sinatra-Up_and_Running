4edcba1025efbf769e20b92603ccc1a818615466
Jeff Weiss <jeff.weiss@puppetlabs.com>
2012-09-26 10:18:45 -0700

    added example 5.5

--- ch05/lib/blog.rb
+++ ch05/lib/blog.rb
@@ -1,9 +1,30 @@
 require 'sinatra/base'
+require 'ostruct'
+require 'time'
 
 class Blog < Sinatra::Base
-  # File.expand_path generates an absolute path.
-  # It also take a patch as a second argument. The
-  # generated path is treated as being relative
-  # to that path.
   set :root, File.expand_path('../../', __FILE__)
+
+  # loop through all the article files
+  Dir.glob "#{root}/articles/*.md" do |file|
+    # parse metadata and content from file
+    meta, content = File.read(file).split("\n\n", 2)
+
+    # generate a metadata object
+    article = OpenStruct.new YAML.load(meta)
+
+    # convert the date to a time object
+    article.date = Time.parse article.date.to_s
+
+    # add the content
+    article.content = content
+
+    # generate a slug for the url
+    article.slug = File.basename file, '.md'
+
+    # set up the route
+    get "/#{article.slug}" do
+      erb :post, :locals => { :article => article }
+    end
+  end
 end
