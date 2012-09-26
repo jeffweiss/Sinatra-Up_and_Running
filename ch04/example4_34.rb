006c75d76a2ee499b5e9255ba13fb6aede7873e2
Jeff Weiss <jeff.weiss@puppetlabs.com>
2012-09-26 09:56:52 -0700

    added example 4.34

---
+++ ch04/example4_34.rb
@@ -0,0 +1,25 @@
+require 'sinatra/base'
+require 'date'
+
+module MyHelpers
+  def time
+    Time.now.to_s
+  end
+end
+
+class MyApplication < Sinatra::Base
+  enable :logging
+
+  helpers MyHelpers
+
+  helpers do
+    def date
+      Date.today.to_s
+    end
+  end
+
+  get('/') { "it's #{time}\n" }
+  get('/today') {"today is #{date}\n" }
+
+  run!
+end
